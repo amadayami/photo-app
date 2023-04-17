bac89aca529f71cb8b7dd3c91927282b9d33e07d
amadayami <amadayamichael@gmail.com>
2023-04-17 17:34:10 -0600

    added bootstrap, jquery, and popper.js. generated files for welcome page.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,26 @@
-const { webpackConfig } = require('shakapacker')
+const webpack = require('webpack')
+const { webpackConfig, merge } = require('shakapacker')
+
+const sassRule = webpackConfig.module.rules.find(rule => rule.test.test('.sass') || rule.test.test('.scss'))
+const sassLoader = sassRule.use.find(use => use.options?.sassOptions)
+const sassOptions = sassLoader.options.sassOptions
+sassOptions.quietDeps = true
 
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
 
-module.exports = webpackConfig
+const customConfig = {
+	// We'll put stuff in here later, such as `plugins: [ … ]` or `resolve: { … }`
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ],
+
+  resolve: {
+    extensions: ['.css']
+  }
+}
+
+module.exports = merge(webpackConfig, customConfig)
